fcdaed8b4b37f277021832713a22d29a7caef4b1
ZexuanHuang <jeffhuang1024@gmail.com>
2018-04-14 22:26:31 -0400

    init

--- reactjs/App1.jsx
+++ reactjs/App1.jsx
@@ -1,12 +1,12 @@
 import React from "react"
 import { render } from "react-dom"
 
-import App1Container from "./containers/App1Container"
+import Header from "./components/Header"
 
 class App1 extends React.Component {
   render() {
     return (
-      <App1Container />
+      <Header />
     )
   }
 }
