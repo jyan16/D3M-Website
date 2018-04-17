71f9a579053e38fc9bbfd0a2daf65973e8d7f323
ZexuanHuang <jeffhuang1024@gmail.com>
2018-04-16 22:56:52 -0400

    React envolved from ancient  0.14.7 to modorn life 16.3.2

--- webpack.local.config.js
+++ webpack.local.config.js
@@ -23,7 +23,16 @@ config.plugins = config.plugins.concat([
 ])
 
 config.module.loaders.push(
-  { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot', 'babel'] }
+  { test: /\.jsx$/,
+    loaders: ['babel'],
+    exclude: /node_modules/,
+    options: {
+        cacheDirectory: true,
+        plugins: [
+            'react-hot-loader/babel'
+        ]
+    }
+  }
 )
 
 config.module.loaders.push(
