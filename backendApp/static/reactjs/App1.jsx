6727ba0b53cc14cd117e612a64c4e2baabf15c1e
jyan16 <jin_yan@brown.edu>
2018-04-25 13:54:18 -0400

    init

--- backendApp/static/reactjs/App1.jsx
+++ backendApp/static/reactjs/App1.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { render } from "react-dom"
 
-import Header from "./components/Header"
+// import Header from "./components/Header"
 import Filter from "./components/Filter"
 import Search from "./components/Search"
 import MainContent from "./components/MainContent"
@@ -20,9 +20,9 @@ class App1 extends React.Component {
     render() {
         return (
             <div id="app-container">
-                <div id="header-container">
-                    <Header/>
-                </div>
+                {/*<div id="header-container">*/}
+                    {/*<Header/>*/}
+                {/*</div>*/}
                 <div id="page-wrapper">
                     <div id="left-toolbar-container">
                         <Filter rootThis={this}/>
