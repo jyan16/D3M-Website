11f0a8f607fd8f28e00081fd2fed5462a14303d7
jyan16 <jin_yan@brown.edu>
2018-04-28 15:13:32 -0400

    test & home page resize

--- backendApp/tests.py
+++ backendApp/tests.py
@@ -1,12 +1,35 @@
-# from django.test import TestCase
-# from .models import *
-#
-#
-# # Create your tests here.
-# class DataSetTestCase(TestCase):
-#     def setUp(self):
-#         DataSet.objects.create(
-#             name="num1",
-#             most_recent_time=
-#         )
-#         Animal.objects.create(name="cat", sound="meow")
+from django.test import TestCase, Client
+from .test_utils import *
+import json
+
+
+# Create your tests here.
+class ModelTestCase(TestCase):
+    def setUp(self):
+        set_up()
+
+    def test_object_saved(self):
+        # records save test
+        records = Record.objects.filter(method='baseline').order_by('score')
+        self.assertEqual(records.first().score, 0.13)
+        self.assertEqual(records.count(), 2)
+
+        # result query test
+        results = DataSet.objects.get(name='num1').result_set.all()
+        self.assertEqual(results.count(), 1)
+
+
+class RequestTestCase(TestCase):
+    def setUp(self):
+        set_up()
+
+    def test_get_all_request(self):
+        c = Client()
+        response = c.get('/all/')
+        response = json.loads(response.content)
+        self.assertEqual(response['ok'], True)
+        self.assertEqual(len(response['data']), 1)
+        self.assertEqual(response['statistic']['classification'][0]['baseline'], 0.13)
+
+
+
