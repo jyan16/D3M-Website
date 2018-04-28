ed15a6c06d0cb30ad00e92ef34db3c9a24dedb28
jyan16 <jin_yan@brown.edu>
2018-04-28 13:58:41 -0400

    deploy

--- backendApp/tests.py
+++ backendApp/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
-
-# Create your tests here.
+# from django.test import TestCase
+# from .models import *
+#
+#
+# # Create your tests here.
+# class DataSetTestCase(TestCase):
+#     def setUp(self):
+#         DataSet.objects.create(
+#             name="num1",
+#             most_recent_time=
+#         )
+#         Animal.objects.create(name="cat", sound="meow")
