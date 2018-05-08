5778c0c317fc33fa6525ac9d5387079d052d966f
zzhang83 <zhiwei_zhang@brown.edu>
2018-05-07 21:52:39 -0400

    up

--- backendApp/static/js/index.js
+++ backendApp/static/js/index.js
@@ -1,6 +1,7 @@
 let i = 0;
 let txt = 'DMhub changes the way you look at data ...';
 let speed = 80;
+
 $(document).ready(function a(){
     if (i < txt.length) {
         document.getElementById("demo").innerHTML += txt.charAt(i);
@@ -9,17 +10,18 @@ $(document).ready(function a(){
     }
 });
 
-// $('#id_docfile').change(function() {
-//     let file = $('#id_docfile')[0].files[0].name;
-//     document.getElementById("filechoosen").innerHTML = file
-// });
+$(':file').change(function() {
+    let file = $('#id_docfile')[0].files[0].name;
+    console.log("ffuck", file)
+    document.getElementById("filechoosen").innerHTML = file
+});
 
-var input = document.getElementById( 'id_docfile' );
-var infoArea = document.getElementById( 'filechoosen' );
-input.addEventListener( 'change', showFileName );
-function showFileName( event ) {
-
-  var input = event.srcElement;
-  var fileName = input.files[0].name;
-  infoArea.textContent = 'File name: ' + fileName;
-}
+// var input = document.getElementById( 'id_docfile' );
+// var infoArea = document.getElementById( 'filechoosen' );
+// input.addEventListener( 'change', showFileName );
+//
+// function showFileName( event ) {
+//   var input = event.srcElement;
+//   var fileName = input.files[0].name;
+//   infoArea.textContent = 'File name: ' + fileName;
+// }
